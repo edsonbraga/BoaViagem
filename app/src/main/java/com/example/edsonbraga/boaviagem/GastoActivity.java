acba075759b1ea3d9af6cc9a890cd28c943dc598
Edson Braga <edsoncbraga@outlook.com>
2016-08-07 08:33:31 -0300

    - Completando a implementação da activity Gasto

---
+++ app/src/main/java/com/example/edsonbraga/boaviagem/GastoActivity.java
@@ -0,0 +1,68 @@
+package com.example.edsonbraga.boaviagem;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+
+import java.util.Calendar;
+
+/**
+ * Created by Edson Braga on 30/07/2016.
+ */
+public class GastoActivity extends Activity {
+
+    private int ano, mes, dia;
+    private Button dataGasto;
+    private Spinner categoria;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gasto);
+
+        // busca a data atual para mostrar no botão
+        Calendar calendar = Calendar.getInstance();
+        ano = calendar.get(Calendar.YEAR);
+        mes = calendar.get(Calendar.MONTH);
+        dia = calendar.get(Calendar.DAY_OF_MONTH);
+
+        dataGasto = (Button) findViewById(R.id.data);
+        dataGasto.setText(dia + "/" + mes + "/" + ano);
+
+        ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(
+                        this, R.array.categoria_gasto,
+                        android.R.layout.simple_spinner_item);
+        categoria = (Spinner) findViewById(R.id.categoria);
+        categoria.setAdapter(adapter);
+    }
+
+    public void selecionarData(View view) {
+        showDialog(view.getId());
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if(R.id.data == id)
+            return new DatePickerDialog(this, listener, ano, mes, dia);
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker,
+                              int year, int monthYear, int dayOfMonth) {
+            ano = year;
+            mes = monthYear;
+            dia = dayOfMonth;
+
+            dataGasto.setText(dia + "/" + mes + "/" + ano);
+        }
+    };
+}
